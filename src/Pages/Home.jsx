0a425c816a018139ebfb4ff6c3c2e0e293273b74
Nikhil singh <nikhilsingh675naman@gmail.com>
2025-01-10 12:17:02 +0530

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,9 +4,11 @@ import TopDeals from "../componants/TopDeals";
 import ProductBanner from "../componants/DiscountBanner";
 import BrandsWeDistribute from "../componants/BrandDistribute";
 import Slider from "../components/Slider";
+import Imageslider from "./Imageslider";
 const Home = () => {
   return (
     <>
+      {<Imageslider />}
       {<Slider />}
       {<PopularProducts />}
       {<TopDeals />}

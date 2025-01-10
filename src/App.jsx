e0b17fbd732e09ad1cbcf1442dcbde2b0244b077
Harsh356a <harsh@365aitech.com>
2025-01-10 13:39:47 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
-
 import Profile from "./Pages/Profile";
-
 import Topbar from "./components/Topbar";
 import Navbar from "./components/Navbar";
 import Shop from "./components/Shop";
@@ -12,7 +10,7 @@ import Cart from "./components/Cart";
 import Account from "./components/Account";
 import Category from "./components/Category";
 // import Footer from "./components/Footer";
-import Products from "./components/Product";
+// import Products from "./components/Product";
 const App = () => {
   return (
     <>
@@ -27,7 +25,7 @@ const App = () => {
         <Route path="/profile" element={<Profile />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/category" element={<Category />} />
-        <Route path="/product/:id" element={<Products />} />
+        {/* <Route path="/product/:id" element={<Products />} /> */}
       </Routes>
       {/* <Footer /> */}
     </>

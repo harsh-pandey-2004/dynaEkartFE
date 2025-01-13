98bab54f3e0e27e090d217614daed0af56f126ad
mishrapriyanshu0103 <mishrapriyanshu0103>
2025-01-13 17:49:15 +0530

    Design Cart Page

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,37 +1,73 @@
+import React from "react";
+import { FaCheck } from "react-icons/fa6";
+import { GrAddCircle, GrSubtractCircle } from "react-icons/gr";
+
 const Cart = () => {
   return (
     <>
       <div className="container mx-auto px-4 sm:px-6 lg:px-16 py-12">
         <h1 className="text-3xl font-bold">Shoping Cart</h1>
-        <div className="flex justify-center pt-6">
-          <div className="w-1/2">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non
-            aliquid possimus fugiat, eaque quisquam illo vel nulla, enim
-            doloribus impedit ex dolores. Amet, rerum natus iste dolore
-            adipisci, accusantium officiis at, consequuntur sint impedit
-            aspernatur. Officiis quae iure quam eos blanditiis voluptatibus
-            perspiciatis maxime? Recusandae maxime eos, ipsam fugiat impedit
-            odio fugit neque velit cupiditate doloribus? Adipisci aspernatur
-            repudiandae doloribus veritatis esse dolore est nostrum consequatur.
-            Natus eius porro error tempore explicabo recusandae amet eum
-            incidunt laborum voluptatum delectus, veniam sunt atque, nam et cum
-            necessitatibus itaque sint accusantium optio nobis ab eos! Animi
-            quis, commodi reprehenderit temporibus architecto amet, earum
-            dignissimos nemo aperiam tenetur consectetur repudiandae libero
-            exercitationem labore debitis magni hic veritatis eius ipsa omnis
-            assumenda eveniet quisquam! Provident, quisquam? Rerum, optio
-            commodi pariatur quia culpa quaerat atque maiores est nostrum
-            voluptatem aliquid nisi saepe quibusdam accusantium maxime obcaecati
-            cupiditate fugit quod debitis at corrupti cumque corporis. Esse
-            eaque nemo quis numquam deleniti eum modi cum molestiae porro
-            laudantium aliquid blanditiis libero ex fugit harum, quae impedit
-            molestias illo totam excepturi fugiat, ipsum reprehenderit!
-            Exercitationem autem maxime natus ipsa nisi temporibus, obcaecati
-            maiores asperiores officia. Officia officiis quibusdam neque earum
-            aperiam, voluptatem corporis rem deleniti, autem doloribus veniam.
+        <div className="flex flex-col md:flex-row justify-center pt-6 gap-5">
+          <div className="md:w-1/2 border-t-2 border-b-2 flex items-center gap-4 p-6">
+            <div className="w-full md:w-1/2">
+              <img
+                src="https://i.ibb.co/m6ZN7LX/soundBox.webp"
+                alt=""
+                className="w-full border-2 border-blue-100 rounded-lg"
+              />
+            </div>
+            <div className="w-full">
+              <h1 className="text-md xl:text-lg font-semibold">
+                Divoom Tivoo Portable Bluetooth Speaker Smart Clock Alarm Pixel
+                Art DIY By App L
+              </h1>
+              <p className="text-md mt-2 font-semibold">
+                <span className="font-normal">Brand: </span> Samsung
+              </p>
+              <p className="text-md mt-1 font-semibold">
+                <span className="font-normal">Category: </span>
+                Mobile
+              </p>
+              <div className="flex my-2 gap-6">
+                <p className="font-semibold text-blue-600">$600</p>
+                <GrAddCircle className="bg-gray-100 rounded-full size-6" />
+                <p className="font-semibold text-lg">1</p>
+                <GrSubtractCircle className="bg-gray-100 rounded-full size-6" />
+              </div>
+              <div className="flex">
+                <FaCheck className="text-green-600 mt-1" />
+                <p className="text-md">In Stock</p>
+              </div>
+              <p className="text-md text-green-600 font-medium">
+                You are saving $20.00 upon purchase
+              </p>
+            </div>
           </div>
-          <div className="bg-gray-100 w-1/2 m-10 p-4 rounded-xl">
-            <h4 className="font-bold text-xl">Order Summary :</h4>
+          <div className="bg-gray-100 md:w-1/2 py-6 px-6 lg:px-20 rounded-xl">
+            <h4 className="font-bold text-2xl md:text-3xl">Order Summary :</h4>
+            <div className="flex justify-between my-4 border-b-2 pb-2">
+              <p className="text-gray-700">Subtotal</p>
+              <p className="text-black font-semibold">$600.00</p>
+            </div>
+            <div className="flex justify-between  my-4 border-b-2 pb-2">
+              <p className="text-gray-700">Shipping estimate</p>
+              <p className="text-black font-semibold">$25.00</p>
+            </div>
+            <div className="flex justify-between  my-4 border-b-2 pb-2">
+              <p className="text-gray-700">Tax estimate</p>
+              <p className="text-black font-semibold">$15.00</p>
+            </div>
+            <div className="flex justify-between  my-4 border-b-2 pb-2">
+              <p className="text-black font-bold">Total Discount</p>
+              <p className="text-black font-semibold">$20.00</p>
+            </div>
+            <div className="flex justify-between  my-4">
+              <p className="text-black text-xl font-bold">Order Total</p>
+              <p className="text-black text-xl font-bold"> $620.00</p>
+            </div>
+            <button className="w-full bg-gray-700 hover:bg-gray-600 text-white py-2 rounded-md">
+              BUY NOW
+            </button>
           </div>
         </div>
       </div>
